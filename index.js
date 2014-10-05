daca385a11e75e833af6696b070b5811223dc1b0
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-10-04 20:30:43 -0400

    Work now please

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/test', function(request, response) {
 app.post('/handleSMS', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
   var resp = new twilio.TwimlResponse();
-  resp.message(req.body.body);
+  resp.message(req.body.Body + " " + req.body.From);
   res.writeHead(200, {
         'Content-Type':'text/xml'
     });
